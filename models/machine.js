1c83a50be14e08cb6ff0b6aaf5adf0d92934c40a
Ashwin Hamal <ahamal@us.ibm.com>
2016-09-20 13:41:04 -0400

    Removed 'path' replaced with 'cwd'

--- models/machine.js
+++ models/machine.js
@@ -14,7 +14,6 @@ var CogSchema = new mongoose.Schema({
   host: String,
   port: String,
 
-  path: String,
   cwd: String,
   run: String,
   args: [String],
@@ -94,7 +93,7 @@ MachineSchema.methods.updateCog = function(c, next) {
   // Update
   _.each([
     'type', 'tags',  'description', 'pid', 'host', 'port',
-    'path', 'run', 'args', 'status', 'exitCode', 'cwd'
+    'run', 'args', 'status', 'exitCode', 'cwd'
   ], (k) => {
     if (c[k]) cog[k] = c[k];
   });
