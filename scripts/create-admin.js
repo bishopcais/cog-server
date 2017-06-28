552bb51e1ee2f2a0ad2fb98fd97add0b54a7b684
Matthew Peveler <matt.peveler@gmail.com>
2017-06-28 12:38:19 -0400

    use mongoose 4.10.x as 4.11 issues error

--- scripts/create-admin.js
+++ scripts/create-admin.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 var settings = require('../settings'),
-  User = require('../models/user');
+    User = require('../models/user');
 
 
 // Creates an admin user with username 'admin', and password 'password'
@@ -10,39 +10,26 @@ console.log('Connecting...')
 
 mongoose.connect(settings.db)
 
-mongoose.connection.on('error',
-  console.error.bind(console, 'connection error:')
-);
-
-mongoose.connection.once('open', () => {
-  var user = new User({
-    username: 'admin',
-    password: 'password',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    isAdmin: true,
-    keys: [
-      {
-        key: "key"
-      }
-    ]
-  });
-
-  console.log('Creating admin...')
-  user.save({}, (err) => {
-    if (err) {
-      // Don't consider it an error that the admin already exists
-      if (err['name'] == 'MongoError' && err['code'] == 11000) {
-        console.log('admin already exists.');
-      }
-      else {
-        console.error('Error creating admin: ', err);
-      }
+var user = new User({
+  username: 'admin',
+  password: 'password',
+  name: 'Admin',
+  email: 'admin@admin.com',
+  isAdmin: true,
+  keys: [
+    {
+      key: "key"
+    }
+  ]
+});
+user.save(function(err) {
+  if (err) {
+    if (err['name'] == 'MongoError' && err['code'] == 11000) {
+      console.log('admin already exists');
     }
     else {
-      console.log('admin created.');
+      console.log('Error creating admin: ', err);
     }
-    mongoose.connection.close();
-    process.exit();
-  });
+  }
+  process.exit();
 });
